cc85747506cfc14a9afa0690d66bf2ce282aed19
Collin John Day <dayc99@elvis.rowan.edu>
2014-11-01 18:40:17 -0400

    First commit

--- primenumbers.py
+++ primenumbers.py
@@ -1,23 +1,30 @@
 
-i = 2
-a = 2
-print('Enter 0 to End')
-num = int(input("Enter a number:"))
-pri = False
+#i = 2
+#a = 2
+#print('Enter 0 to End')
+#num = int(input("Enter a number:"))
+#pri = False
 
-while num != 0:
+def prime(num):
+    i = 2
+    a = 2
+    pri = False
     
-    while num >= i:
+    while num != 0:
+    
+        while num >= i:
 
-        while i>a:
-            if i%a==0:
-                pri = True
-            a +=1
+            while i>a:
+                if i%a==0:
+                    pri = True
+                a +=1
 
-        if pri == False:
-            print(str(i))
-        i+=1
-        a=2
-        pri = False
-    num = int(input("Enter a number:"))
-    i=2
+            if pri == False:
+                print(str(i))
+            i+=1
+            a=2
+            pri = False
+        print('Enter 0 to End')
+        num = int(input("Enter a number:"))
+        i=2
+    
