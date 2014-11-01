e16468f68a9cfb84198f870d4bffd94f9256b247
Collin John Day <dayc99@elvis.rowan.edu>
2014-11-01 18:26:52 -0400

    First commit

--- primenumbers.py
+++ primenumbers.py
@@ -1,24 +1,23 @@
 
 i = 2
+a = 2
 print('Enter 0 to End')
 num = int(input("Enter a number:"))
 pri = False
 
 while num != 0:
-    while num > i:
-        if num%i==0:
-            pri = True
-            print(str(i) + ', ')
-        i +=1
-
-    if pri == False:
-        print(str(num) + ' is Prime')
-    else:
-        print(str(num) + ' is Not Prime')
-
-    num = int(input("Enter a number:"))
-    pri = False
-    i = 2
     
+    while num >= i:
 
+        while i>a:
+            if i%a==0:
+                pri = True
+            a +=1
 
+        if pri == False:
+            print(str(i))
+        i+=1
+        a=2
+        pri = False
+    num = int(input("Enter a number:"))
+    i=2
